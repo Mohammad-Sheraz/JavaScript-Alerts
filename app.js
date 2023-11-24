4844edad111640860354a717ddd4eb585ef2b194
Mohammad-Sheraz <muhammadsheraz5081999@gmail.com>
2023-11-24 19:50:13 +0500

    Assignment 2

---
+++ app.js
@@ -0,0 +1,43 @@
+var userName = 'Muhammad Sheraz'
+var myName = 'Muhammad Sheraz'
+
+var message = 'Hello World'
+alert(message)
+
+var age = '17 years old'
+var uni = 'SSUET'
+var skill = 'Mobile and Cloud Computing - Module A'
+
+alert(myName)
+alert(age)
+alert(uni)
+alert(skill)
+
+var pizza = 'PIZZA\nPIZZ\nPIZ\nPI\nP'
+alert(pizza)
+
+var numberofchildren = '2 kids.'
+var partnersName = 'ABC'
+var geographicLocation = 'USA,'
+var jobTitle = 'Sofware Developer'
+
+alert("You will be a " + jobTitle + ' in ' + geographicLocation + ' and married to ' + partnersName + ' with ' + numberofchildren )
+
+
+var email = 'muhammadsheraz5081999@gmail.com.'
+
+alert('My email address is ' + email )
+
+var book = 'A smarter way to learn JavaScript'
+
+alert('I am trying to learn from the book ' + '"' + book + '"')
+
+
+var myName2 = 'My name is Muhammad Sheraz'
+var age2 = 'I am 17 years old'
+var skill2 = 'I take Mobile and Cloud Computing - Module A'
+var uni2 = 'I study in SSUET'
+
+alert(myName2 + '\n' + age2 + '\n' + uni2 + '\n' + skill2)
+
+document.write('Yay! I can write HTML content through JavaScript')
